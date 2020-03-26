8167b80ac2f713e5e50c7f1b0d55e7ada5f332fe
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2020-03-26 08:04:30 -0500

    Third Commit

--- src/test/java/coronavirusAPITest/UnirestTest.java
+++ src/test/java/coronavirusAPITest/UnirestTest.java
@@ -13,27 +13,27 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class UnirestTest {
 
-	@Test
-	public void shouldReturnAllCases() throws UnirestException {
-		HttpResponse<JsonNode> response = Unirest
-				.get("https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats")
-				.header("x-rapidapi-host", "covid-19-coronavirus-statistics.p.rapidapi.com")
-				.header("x-rapidapi-key", "34f05cff54msh30ba6f36c91c183p166499jsn555917ef62b8")
-				.asJson();
+	// @Test
+	// public void shouldReturnAllCases() throws UnirestException {
+	// 	HttpResponse<JsonNode> response = Unirest
+	// 			.get("https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats")
+	// 			.header("x-rapidapi-host", "covid-19-coronavirus-statistics.p.rapidapi.com")
+	// 			.header("x-rapidapi-key", "34f05cff54msh30ba6f36c91c183p166499jsn555917ef62b8")
+	// 			.asJson();
 		
-		assertNotNull(response.getBody());
-		assertEquals((int)HttpStatus.SC_OK,response.getStatus());
-	}
+	// 	assertNotNull(response.getBody());
+	// 	assertEquals((int)HttpStatus.SC_OK,response.getStatus());
+	// }
 	
-	@Test
-	public void shouldReturnCaseByCountry() throws UnirestException {
-		HttpResponse<JsonNode> response = Unirest
-				.get("https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=Canada")
-				.header("x-rapidapi-host", "covid-19-coronavirus-statistics.p.rapidapi.com")
-				.header("x-rapidapi-key", "34f05cff54msh30ba6f36c91c183p166499jsn555917ef62b8")
-				.asJson();
+	// @Test
+	// public void shouldReturnCaseByCountry() throws UnirestException {
+	// 	HttpResponse<JsonNode> response = Unirest
+	// 			.get("https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=Canada")
+	// 			.header("x-rapidapi-host", "covid-19-coronavirus-statistics.p.rapidapi.com")
+	// 			.header("x-rapidapi-key", "34f05cff54msh30ba6f36c91c183p166499jsn555917ef62b8")
+	// 			.asJson();
 		
-		assertNotNull(response.getBody());
-		assertEquals((int)HttpStatus.SC_OK,response.getStatus());
-	}
+	// 	assertNotNull(response.getBody());
+	// 	assertEquals((int)HttpStatus.SC_OK,response.getStatus());
+	// }
 }
