36cc911054393719362cfe98e551057d73c0b3ad
ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2020-03-26 21:45:14 -0500

    Correccion click en tabla

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,18 +4,26 @@ var app = (function () {
     var loadTable = function(stats) {
           $("#stats").append(
           stats.map(function(stat){
-            return "<tr><td>" + stat.country + "</td><td>" + stat.deaths + "</td><td>" +stat.infected + "</td><td>" + stat.cured 
+            return "<tr><td>" + stat.country + "</td><td>" + stat.deaths + "</td><td>" +stat.infected + "</td><td>" + stat.cured +"</tr></td>"
             })  
           );
+          $('tr').click(function(){
+            var name = this.cells[0].textContent;
+            apiclient.getProvinceStats(name,loadTable2);
+            $("#CountryN").text(name);
+          });
         }
 
     var loadTable2 = function(stats) {
+        $("#ProvStats").find("tr:gt(0)").remove();
         $("#ProvStats").append(
         stats.map(function(stat){
             return "<tr><td>" + stat.country + "</td><td>" + stat.deaths + "</td><td>" +stat.infected + "</td><td>" + stat.cured 
             })  
         );
+        
     }
+    
     var iniciar = function(){
         apiclient.getAllStats(loadTable);
         apiclient.getProvinceStats("Canada",loadTable2);
